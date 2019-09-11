48bd62178356e89ad8843a9c8f1c48b19b82aff9
ola-szym <szymanowicz.aleksandra@gmail.com>
2019-09-11 20:56:43 +0200

    modified HelloWorld

--- src/main/java/GitHelloWorld.java
+++ src/main/java/GitHelloWorld.java
@@ -1,5 +1,5 @@
 public class GitHelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello git! Let's start the adventure!");
+        System.out.println("I am going to be a good java developer!");
     }
 }
